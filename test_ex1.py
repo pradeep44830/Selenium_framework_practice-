895128d340d52c0fae3b7a0bbe49fba6a20606ec
pradeep44830 <pradeepkumar44830@gmail.com>
2026-05-10 00:23:37 +0530

    ID locators to Xpath locators added

--- test_ex1.py
+++ test_ex1.py
@@ -25,12 +25,12 @@ def test_ex1():
     driver.get("https://www.saucedemo.com/")
     header = driver.find_element(By.CLASS_NAME,"login_logo").text
     assert header == "Swag Labs"
-    driver.find_element(By.ID,"user-name").send_keys("standard_user")
-    driver.find_element(By.ID,"password").send_keys("secret_sauce")
-    driver.find_element(By.ID, "login-button").click()
+    driver.find_element(By.XPATH,"//input[@id='user-name']").send_keys("standard_user")
+    driver.find_element(By.XPATH, "//input[@id='password']").send_keys("secret_sauce")
+    driver.find_element(By.XPATH, "//input[@id='login-button']").click()
     WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.CLASS_NAME, "title"))
+        EC.visibility_of_element_located((By.XPATH, "//span[text()='Products']"))
     )
-    page_menu = driver.find_element(By.CLASS_NAME, "title").text
+    page_menu = driver.find_element(By.XPATH, "//span[text()='Products']").text
     assert page_menu == "Products"
     driver.quit()
